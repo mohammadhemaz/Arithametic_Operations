8c1e2e0a0242b20d1f585e4540795cd616970d57
mohammadhemaz <mohammadhemaz46@gmail.com>
2026-03-11 09:33:50 +0530

    Create Arithametic.java

---
+++ Arithametic.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Arithmatic
+  {
+    public static void main(String args[])
+    {
+      Scanner s= new Scanner(System.in);
+      int a=s.nextInt;int b=s.nextInt;
+      int sum = a+b;
+      int dif = a-b;
+      int mul = a*b;
+      int div = a/b;
+      System.out.println("sum of two no. is="+sum);
+      System.out.println("diffrence b/w two no. = "+dif);
+      System.out.println("multiplication of two no is="+mul)
+      System.out.println("quotiont of two no. is="+div)
+    }
+  }
